43781c702b8dd284e31a0738f177e65e0d62cb66
jocoio <joco.hello@gmail.com>
2023-03-19 11:27:53 -0400

    feat - new nav menu

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,170 +1,126 @@
 <template>
   <div id="nav">
-      <Joco @click.native="togNav"/>  
-      <div id="navContent" ref="content" v-if="open" :style="width_sty">
-        <!-- Top nav elements -->
-        <Collab v-if="this.mode === 'letter'" id="company" :style="company_sty"/>
-        <Controller v-else id="controller" :style="control_sty"/>
-        <!-- Nav content -->
-        <CoverLetter v-if="this.mode === 'letter'" :style="content_sty"/>
-        <Info v-else :style="content_sty"/>
-      </div>
+    <Joco @click.native="togNav" />
+    <div id="navContent" ref="content" v-if="open" :style="width_sty">
+      <!-- Nav content -->
+      <Menu id="menu" :style="control_sty" />
+    </div>
   </div>
 </template>
 
 <script>
-  import anime from 'animejs';
-  import { mapState, mapGetters } from 'vuex';
-  import Info from './Info';
-  import CoverLetter from './CoverLetter';
-  import Joco from '../assets/logos/Joco';
-  import Controller from './Controller';
-  import Collab from './Collab';
+import anime from "animejs";
+import { mapState, mapGetters } from "vuex";
+// import Featured from "./Featured";
+import Joco from "../assets/logos/Joco";
+import Menu from "./Menu";
 
-  export default {
-  name: 'Nav',
-  data: function () {
+export default {
+  name: "Nav",
+  data: function() {
     return {
       animOutro: null,
-      company: null
-    }
+      company: null,
+    };
   },
   components: {
-    Info,
-    CoverLetter,
+    // Featured,
     Joco,
-    Controller,
-    Collab
+    Menu,
   },
   computed: {
     ...mapState([
-      'mode',
-      'naving',
-      'navOpen',
-      'navWidth',
-      'introd',
-      'transitioning',
-      'naving',
+      "mode",
+      "naving",
+      "navOpen",
+      "navWidth",
+      "introd",
+      "transitioning",
+      "naving",
     ]),
-    ...mapGetters([
-      'blockWidth',
-      'blockHeight'
-    ]),
-    open () {
-      return this.navOpen & !this.naving
+    ...mapGetters(["blockWidth", "blockHeight"]),
+    open() {
+      return this.navOpen & !this.naving;
     },
-    width_sty () {
+    width_sty() {
       return {
-        width: this.navWidth + 'px',
-      }
+        width: this.navWidth + "px",
+      };
     },
-    control_sty () {
+    content_sty() {
       return {
-        width: this.blockWidth + 'px',
-        height: this.blockHeight + 'px',
-        top: '-' + this.blockHeight + 'px'
-      }
+        marginTop: -this.blockHeight / 5 + "px",
+        padding: "0 10%",
+      };
     },
-    company_sty () {
-      return {
-        width: this.blockWidth * 2 + 'px',
-        height: this.blockHeight + 'px',
-        top: '-' + this.blockHeight + 'px'
-      }
-    },
-    content_sty () {
-      return {
-        marginTop: (-this.blockHeight / 5) + 'px',
-        padding: '0 10%'
-      }
-    }
   },
   watch: {
-    naving () {
+    naving() {
       // We are naving but want to close
       if (this.naving && !this.navOpen) {
         this.playOutro();
       }
     },
-    transitioning () {
+    transitioning() {
       if (this.transitioning) {
         this.playOutro();
-      }
-      else if (this.mode === "letter") {
+      } else if (this.mode === "letter") {
         setTimeout(() => {
-          this.$store.dispatch('changeNav')
+          this.$store.dispatch("changeNav");
         }, 2300);
       }
-    }
+    },
   },
   methods: {
-    togNav () {
+    togNav() {
       if (this.introd && !this.naving) {
-        this.$store.dispatch('changeNav');
+        this.$store.dispatch("changeNav");
       }
     },
-    playOutro () {
+    playOutro() {
       this.animOutro = anime({
         duration: 250,
         targets: this.$refs.content,
-        easing: 'easeInExpo',
+        easing: "easeInExpo",
         opacity: [1, 0],
-      })
+      });
 
       this.animOutro.play();
-    }
+    },
   },
-  mounted () {}
-}
+  mounted() {},
+};
 </script>
 
 <style scoped>
+#navButtons {
+  width: 100%;
+  height: 100%;
+}
 
-  #navButtons {
-    width: 100%;
-    height: 100%;
-  }
+#navContent {
+  position: absolute;
+  text-align: left;
+  z-index: 1;
+}
 
-  #controller {
-    position: absolute;
-    right: 10%;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-  }
+.icon {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  opacity: 1;
+}
 
-  #company {
-    position: absolute;
-    right: 0;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    margin-left: -10%;
-  }
+.icon > svg {
+  height: 30%;
+}
 
+@media only screen and (max-width: 768px) {
   #navContent {
-    position: absolute;
-    text-align: left;
-    z-index: 1;
+    margin-top: 0px;
   }
-
-  .icon {
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    opacity: 1;
-  }
-
-  .icon>svg {
-    height: 30%;
-  }
-
-  @media only screen and (max-width: 768px) {
-    #navContent {
-      margin-top: 0px;
-    }
-  }
+}
 </style>
